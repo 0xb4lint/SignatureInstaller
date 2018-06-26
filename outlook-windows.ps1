726de5e834e2198725d057413f82be58a31604d4
Szekeres Bálint <szekeres.balint@hps.hu>
2018-06-26 21:32:35 +0200

    Outlook Windows: enable external content, link pictures

--- outlook-windows.ps1
+++ outlook-windows.ps1
@@ -13,5 +13,33 @@ $signatureFile = $FolderLocation + "signature.htm"
 
 $signatureHTML | Out-File -Encoding Unicode $signatureFile
 
+
+IF (Test-Path "HKCU:\Software\Microsoft\Office\14.0") {
+	IF (!(Test-Path "HKCU:\Software\Microsoft\Office\14.0\Outlook\Options\Mail")) {
+		New-Item "HKCU:\Software\Microsoft\Office\14.0\Outlook\Options\Mail" -Force
+	}
+
+	New-ItemProperty "HKCU:\Software\Microsoft\Office\14.0\Outlook\Options\Mail" -Name "BlockExtContent" -Value 0 -PropertyType DWORD -Force
+	New-ItemProperty "HKCU:\Software\Microsoft\Office\14.0\Outlook\Options\Mail" -Name "Send Pictures With Document" -Value 0 -PropertyType DWORD -Force
+}
+
+IF (Test-Path "HKCU:\Software\Microsoft\Office\15.0") {
+	IF (!(Test-Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\Mail")) {
+		New-Item "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\Mail" -Force
+	}
+
+	New-ItemProperty "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\Mail" -Name "BlockExtContent" -Value 0 -PropertyType DWORD -Force
+	New-ItemProperty "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\Mail" -Name "Send Pictures With Document" -Value 0 -PropertyType DWORD -Force
+}
+
+IF (Test-Path "HKCU:\Software\Microsoft\Office\16.0") {
+	IF (!(Test-Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail")) {
+		New-Item "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail" -Force
+	}
+
+	New-ItemProperty "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail" -Name "BlockExtContent" -Value 0 -PropertyType DWORD -Force
+	New-ItemProperty "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail" -Name "Send Pictures With Document" -Value 0 -PropertyType DWORD -Force
+}
+
 ""
 "done."
