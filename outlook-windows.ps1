cd5360e538867e80de7e365621fe4105168a5b9e
Szekeres Bálint <szekeres.balint@hps.hu>
2018-07-09 14:12:28 +0200

    < win10 support

--- outlook-windows.ps1
+++ outlook-windows.ps1
@@ -2,8 +2,14 @@ $UserDataPath = $Env:appdata
 $FolderLocation = $UserDataPath + '\Microsoft\Signatures\'
 
 $url = Read-Host -Prompt 'enter url'
+$osVersion = [System.Environment]::OSVersion.Version.Major
 
-[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls, ssl3"
+IF ($osVersion -ge 10) {
+	[Net.ServicePointManager]::SecurityProtocol = "Tls12, Tls11, Tls, Ssl3"
+} ELSE {
+	[Net.ServicePointManager]::SecurityProtocol = "Tls, Ssl3"
+}
+
 $wc = New-Object System.Net.WebClient
 $wc.Encoding = [System.Text.Encoding]::UTF8
 $signatureHTML = $wc.DownloadString($url)
